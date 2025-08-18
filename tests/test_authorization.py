fe5a6b1b7916b64955c5a52862c539469a17db13
nalivayko_2018@inbox.ru <nalivayko_2018@inbox.ru>
2025-08-18 21:51:26 +0700

    Перезапуски автотестов в Pytest

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -21,3 +21,5 @@ def test_wrong_email_or_password_authorization(chromium_page: Page, email: str, 
         wrong_email_or_password_alert = chromium_page.get_by_test_id("login-page-wrong-email-or-password-alert")
         expect(wrong_email_or_password_alert).to_be_visible()
         expect(wrong_email_or_password_alert).to_have_text("Wrong email or password")
+
+        chromium_page.wait_for_timeout(3000)
