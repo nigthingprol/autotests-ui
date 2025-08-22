76a652e128b96ffaea2ababe0e025de771069430
nalivayko_2018@inbox.ru <nalivayko_2018@inbox.ru>
2025-08-22 20:38:53 +0700

    Практикуемся в реализации PageComponent

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -1,28 +1,17 @@
+from components.authentication.registartion_form_component import RegistrationFormComponent
 from pages.base_page import BasePage
 from playwright.sync_api import Page, expect
 
 # Создаю класс страницы регистрации
 class RegistrationPage(BasePage):
-    def __init__(self, page):
+    def __init__(self, page: Page):
         super().__init__(page)
 
+        self.registration_form = RegistrationFormComponent(page)
+
         # Передаю локаторы на странице регистрации
-        self.registration_email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-        self.registration_username_input = page.get_by_test_id('registration-form-username-input').locator('input')
-        self.registration_password_input = page.get_by_test_id('registration-form-password-input').locator('input')
         self.registration_button = page.get_by_test_id('registration-page-registration-button')
         self.login_page_link = page.get_by_test_id('registration-page-login-link')
-
-    # Метод, заполняющий все поля на странице регистрации
-    def fill_registration_form(self, email: str, username: str, password: str):
-        self.registration_email_input.fill(email)
-        expect(self.registration_email_input).to_have_value(email)
-
-        self.registration_username_input.fill(username)
-        expect(self.registration_username_input).to_have_value(username)
-
-        self.registration_password_input.fill(password)
-        expect(self.registration_password_input).to_have_value(password)
 
     # Метод, кликающий по кнопке Registration
     def click_registration_button(self):
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -9,9 +9,10 @@ from pages.dashboard_page import DashboardPage
 @pytest.mark.parametrize('email, username, password', [('email@mail.ru', 'username', 'password')])
 def test_successful_registration(registration_page: RegistrationPage, dashboard_page: DashboardPage , email: str, username: str, password: str):
     registration_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
-    registration_page.fill_registration_form(email=email, username=username, password=password)
+    registration_page.registration_form.fill(email=email, username=username, password=password)
+    registration_page.registration_form.check_visible(email=email, username=username, password=password)
     registration_page.click_registration_button()
 
     # Проверка видимости заголовка "Dashboard"
-    dashboard_page.check_title_text()
+    dashboard_page.check_visible_dashboard_title()
 
