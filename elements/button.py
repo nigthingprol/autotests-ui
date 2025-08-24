82fb96eb32d590ecdc1dd2ba575752695dc282e1
nalivayko_2018@inbox.ru <nalivayko_2018@inbox.ru>
2025-08-24 14:58:32 +0700

    Знакомство с PageFactory

---
+++ elements/button.py
@@ -0,0 +1,15 @@
+from playwright.sync_api import expect
+from elements.base_element import BaseElement
+
+
+class Button(BaseElement):
+    def check_enabled(self, **kwargs):
+        locator = self.get_locator(**kwargs)
+        expect(locator).to_be_enabled()
+
+    def check_disabled(self, **kwargs):
+        locator = self.get_locator(**kwargs)
+        expect(locator).to_be_disabled()
+    
+    
+    
