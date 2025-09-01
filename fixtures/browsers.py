5a558a922bef139ad5ee5c43adf58fb7d9aa1a6c
nalivayko_2018@inbox.ru <nalivayko_2018@inbox.ru>
2025-09-01 14:11:50 +0700

    Добавление видео в Allure отчет

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -1,24 +1,16 @@
-
 import pytest
 from playwright.sync_api import Page, Playwright
 import allure
 from pages.authentication.registration_page import RegistrationPage
 from _pytest.fixtures import SubRequest
+from tools.playwright.pages import initialize_playwright_page
+
 
 
 # Фикстура, возвращающая страницу 
 @pytest.fixture
 def chromium_page(request: SubRequest, playwright: Playwright) -> Page:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
-    context.tracing.start(screenshots=True, snapshots=True, sources=True)
-
-    yield context.new_page()
-
-    context.tracing.stop(path=f'./tracing/{request.node.name}.zip')
-    browser.close()
-
-    allure.attach.file(f'./tracing/{request.node.name}.zip', name='trace', extension='zip')
+    yield from initialize_playwright_page(playwright, test_name=request.node.name)
 
 # Фикстура, регистрирующая нового юзера + сохраняющая состояние браузера
 @pytest.fixture(scope='session')
@@ -36,18 +28,7 @@ def initialize_browser_state(playwright: Playwright) -> None:
     context.storage_state(path='browser-state.json')
     browser.close()
 
-
 # Фикстура, открывающая новую страницу браузера, используя сохраненное состояние браузера
 @pytest.fixture(scope='function')
 def chromium_page_with_state(request: SubRequest, initialize_browser_state, playwright: Playwright) -> Page:
-    browser = playwright.chromium.launch(headless=False)
-    # Используем ранее сохраненное состояние браузера
-    context = browser.new_context(storage_state='browser-state.json')
-    context.tracing.start(screenshots=True, snapshots=True, sources=True)
-    page = context.new_page()
-    yield page
-    
-    context.tracing.stop(path=f'./tracing/{request.node.name}.zip')
-    browser.close()
-
-    allure.attach.file(f'./tracing/{request.node.name}.zip', name='trace', extension='zip')
+    yield from initialize_playwright_page(playwright, test_name=request.node.name, storage_state='browser-state.json')
