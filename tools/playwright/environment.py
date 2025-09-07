526ddee3934a1e8b714fa8477133a4aeec29a280
nalivayko_2018@inbox.ru <nalivayko_2018@inbox.ru>
2025-09-07 13:48:19 +0700

    Добавляем информацию о переменных окружения в Allure отчет

--- tools/playwright/environment.py
+++ tools/playwright/environment.py
@@ -1,7 +1,16 @@
+import sys
 from config import settings
+import platform
 
 def create_allure_environment_file():
     items = [f'{key}={value}' for key, value in settings.model_dump().items()]
+
+    os_info = f'{platform.system()}, {platform.release()}'
+    items.append(f'os_info={os_info}')
+
+    python_version = sys.version
+    items.append(f'python_version={python_version}')
+
     properties = '\n'.join(items)
 
     with open(settings.allure_results_dir.joinpath('environment.properties'), 'w+') as file:
